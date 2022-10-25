423f418e53d55db74ff348212fc554deaef5900a
vitoormt <84139448+vitoormt@users.noreply.github.com>
2022-10-25 11:27:58 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+//renderização front-end
+app.use(express.json())
+app.set('view engine','ejs')
+app.set('view engine','html')
+
+//exportar os dados de conexão do servidor
+module.exports = {app,port}
+//multer
+const multer = require('multer')
+//storage
+const storage = multer.diskStorage({
+    //destino
+    destination: (req, res, cb) => {
+        cb(null,'uploads')
+    },
+    //nome do arquivo
+    filename: (req, file, cb) => {
+        cb(null,file.originalname)
+    },
+})
+
+const upload = multer ({storage})
+
+//rota padrão
+app.get('/', (req,res)=>{
+    res.render('index.ejs')
+})
+
+//upload
+//midleware
+app.post('/',upload.single('arquivo'), (req,res) => {
+    res.send("OK")
+    console.log(req.body,req.file) //apoio a lógica
+})
+
+//escuta do servidor
+app.listen(port, () => {
+    console.log(`Servidor escutando em http://localhost:${port}`)
+})
